f10e1327638929b9b943848f17f826ea7963f132
Mesterhazy001 <gengsztermesterhazy@gmail.com>
2026-07-05 14:03:04 +0200

    0
    0

---
+++ modules/szamolo.py
@@ -0,0 +1,12 @@
+# C:\GGScript\Modules\szamolo.py
+
+# Egy Python változó
+VERZIO = "Python-Modul v1.0"
+
+# Egy egyszerű Python függvény
+def py_osszead(a, b):
+    return a + b
+
+# Egy komplexebb Python függvény
+def py_koszones(nev):
+    return f"Üdvözöllek a Python modulból, {nev}!"
